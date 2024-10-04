22c1ef1323affe23a31b70e54fd3c231faf45c7c
robson-carvalho <robsoncarvalho.tech@gmail.com>
2024-10-04 15:38:49 -0300

    feat: desenvolvimento finalizado

--- main.py
+++ main.py
@@ -2,28 +2,72 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class Perceptron:
-    # Inicialização com valores aleatórios de pesos aleatórios e baia 
+    # Inicialização os pesos e o limiar com values aleatórios
     def __init__(self):
         self.weights = np.random.uniform(-1, 1, 3)
-        self.bias = np.random.uniform(-1, 1)
-    
-    # Treinamento 
+        self.bias = np.random.uniform(-1,1)
+
+    # Função de ativação degrau bipolar
+    def bipolarStepActivationFunction(self, u):
+        if u >= 0:
+            return 1
+        return -1
+
+    # Treinamento
     def training(self, inputs, outputs, learning_rate, epochs):
-        self.inputs = inputs # Entradas 
-        self.outputs = outputs # Saídas esperadas
-        self.learning_rate = learning_rate # Taxa de aprendizagem
-        self.epochs = epochs # Número de épocas
-        
+        self.inputs = inputs  # Entradas
+        self.outputs = outputs  # Saídas esperadas
+        self.learning_rate = learning_rate  # Taxa de aprendizagem
+        self.epochs = epochs  # Número de épocas
+
         for _ in range(epochs):
             for i in range(len(inputs)):
-                deltaWeight = learning_rate * inputs[i] * outputs[i]
-                deltaBias = learning_rate * outputs[i]
-                self.weights[i] += deltaWeight
-                self.bias = self.bias + deltaBias
-                
+                x = inputs[i]  # uma lista de amostra, nesse caso, x¹, x² e x³ da posição i.
+
+                u = (np.dot(x, self.weights) + self.bias)  # Somatório de x*w somado ao limiar 
+
+                y = self.bipolarStepActivationFunction(u) # Função de ativação
+
+                # Se y for diferente da saída deseja, atualizamos os pesos e limiar 
+                if y != outputs[i]:
+                    error = (outputs[i] - y)
+                    self.weights[0] += (learning_rate * error * inputs[i][0])
+                    self.weights[1] += (learning_rate * error * inputs[i][1])
+                    self.weights[2] += (learning_rate * error * inputs[i][2])
+                    self.bias += (learning_rate * error * -1)
+
+
+    # Função de operação para classificar amostras após o treinamento
+    def operation(self, inputs):
+        u = (np.dot(inputs, self.weights) + self.bias)
+
+        y = self.bipolarStepActivationFunction(u)
         
+        return y
 
         
+inputs = []
+outputs = []  
+
+with open('training.txt', 'r') as file:
+    for line in file:
+        values = line.split()
         
-        
-        
+        if len(values) >= 5:  
+            data = [float(values[i]) for i in range(1, 4)]  
+            inputs.append(data)  
+            outputs.append(float(values[4]))
+
+model = Perceptron()
+model.training(inputs, outputs, 0.01, 100)
+
+tests = [[-0.3565, 0.0620, 5.9891], [-0.7842, 1.1267, 5.5912]] 
+results = [-1,1]
+
+for i in range(0, len(tests)):
+    v = model.operation(tests[i])
+    
+    print(f"Saida {v}")
+    print(f"Resultado esperado {results[i]}")
+
+
