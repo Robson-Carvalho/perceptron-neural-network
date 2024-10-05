a86f86d7d139bb10638012a3374f8bab8c235d45
robson-carvalho <robsoncarvalho.tech@gmail.com>
2024-10-04 23:20:52 -0300

    feat: número de épocas é encontrado, não passado como parâmetro.

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import numpy as np
 class Perceptron:
     # Inicialização os pesos e o limiar com values aleatórios
     def __init__(self):
-        self.weights = np.random.uniform(-1, 1, 3)
-        self.bias = np.random.uniform(-1,1)
+        self.weights = np.random.uniform(0, 1, 3)
+        self.bias = np.random.uniform(0,1)
+        self.epochs = 0
 
     # Função de ativação degrau bipolar
     def bipolarStepActivationFunction(self, u):
@@ -14,28 +15,37 @@ class Perceptron:
         return -1
 
     # Treinamento
-    def training(self, inputs, outputs, learning_rate, epochs):
+    def training(self, inputs, outputs, learning_rate):
         self.inputs = inputs  # Entradas
         self.outputs = outputs  # Saídas esperadas
         self.learning_rate = learning_rate  # Taxa de aprendizagem
-        self.epochs = epochs  # Número de épocas
+        
+        # Setup para visualização do treinamento
 
-        for _ in range(epochs):
-            for i in range(len(inputs)):
-                x = inputs[i]  # uma lista de amostra, nesse caso, x¹, x² e x³ da posição i.
+        # Aplicação do algoritmo supervisionado de Hebb (regra de Hebb)
+        error = 1
+        while error != 0:
+            self.epochs += 1 # Atualização da quantidade de épocas
+
+            for i in range(len(inputs)): # Para cada amostra de entrada
+                x = inputs[i]  # uma lista de amostra, nesse caso, x¹, x² e x³ da posição i
 
                 u = (np.dot(x, self.weights) + self.bias)  # Somatório de x*w somado ao limiar 
 
                 y = self.bipolarStepActivationFunction(u) # Função de ativação
 
+                error = (outputs[i] - y) # Calculando erro
+
                 # Se y for diferente da saída deseja, atualizamos os pesos e limiar 
                 if y != outputs[i]:
-                    error = (outputs[i] - y)
                     self.weights[0] += (learning_rate * error * inputs[i][0])
                     self.weights[1] += (learning_rate * error * inputs[i][1])
                     self.weights[2] += (learning_rate * error * inputs[i][2])
-                    self.bias += (learning_rate * error * -1)
+                    self.bias += (learning_rate * error * (-1))
 
+        # Configuração para visualização do treinamento
+        
+         
 
     # Função de operação para classificar amostras após o treinamento
     def operation(self, inputs):
@@ -59,7 +69,7 @@ with open('training.txt', 'r') as file:
             outputs.append(float(values[4]))
 
 model = Perceptron()
-model.training(inputs, outputs, 0.01, 100)
+model.training(inputs, outputs, 0.01)
 
 tests = []
 results = []  
@@ -73,11 +83,14 @@ with open('test.txt', 'r') as file:
             tests.append(data)  
             results.append(float(values[4]))
 
+print(f"Quantidade de épocas: {model.epochs}\n")
+
 for i in range(0, len(tests)):
     v = model.operation(tests[i])
     print(f"Teste {i+1}")
     print(f"Saida: {v}")
     print(f"Resultado esperado: {results[i]}\n")
+
 
 
 
